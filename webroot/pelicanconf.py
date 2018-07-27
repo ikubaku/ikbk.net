61275945918b96832c2cebbce18547078eee931e
Hideyuki Teramura <hide4d51@gmail.com>
2018-07-28 03:00:48 +0900

    Add pelican files

---
+++ webroot/pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ikubaku'
+SITENAME = 'ikbk.net'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Plugins
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['asciidoc_reader']
+
+## asciidoc_reader settings
+ASCIIDOC_CMD = "asciidoctor"
