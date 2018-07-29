9988b7336edaf0da147e65077101087fe0c3aac8
Hideyuki Teramura <hide4d51@gmail.com>
2018-07-29 19:25:02 +0900

    Update configuration

--- webroot/pelicanconf.py
+++ webroot/pelicanconf.py
@@ -10,7 +10,8 @@ PATH = 'content'
 
 TIMEZONE = 'Asia/Tokyo'
 
-DEFAULT_LANG = 'ja'
+#DEFAULT_LANG = 'ja'
+LOCALE = 'en_US.UTF-8'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,14 +21,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Kyoto Institute of Technology', 'https://www.kit.ac.jp/'),
+         ('KMC', 'https://www.kmc.gr.jp/'),
+         ('KITCC', 'http://www.kitcc.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub(TBD)', '#'),
+          ('Twitter', 'https://twitter.com/ikubaku10'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -40,3 +40,90 @@ PLUGINS = ['asciidoc_reader']
 
 ## asciidoc_reader settings
 ASCIIDOC_CMD = "asciidoctor"
+
+# Theme
+THEME = "../pelican-themes/nest"
+
+# nest theme settings
+SITESUBTITLE = u'hoge'
+NEST_CSS_MINIFY = True
+
+## Header
+MENUITEMS = [('HOME', '/'), ('About', '/about.html'), ('Category', '/categories.html'), ('Info', '/info.html')]
+NEST_HEADER_IMAGE = ''
+NEST_HEADER_LOGO = ''
+
+## Footer
+NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
+NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html'), ('Authors','/authors.html')]
+NEST_SITEMAP_ATOM_LINK = u'Feed'
+NEST_SOCIAL_COLUMN_TITLE = u'Meet me at:'
+NEST_LINKS_COLUMN_TITLE = u'Links'
+NEST_COPYRIGHT = u'&copy; Ikubaku 2018. All rights reserved.'
+
+## index.html
+NEST_INDEX_HEAD_TITLE = u'Home'
+NEST_INDEX_HEADER_TITLE = u'ikbk.net'
+NEST_INDEX_HEADER_SUBTITLE = u''
+NEST_INDEX_CONTENT_TITLE = u'Latest News'
+
+## archives.html
+NEST_ARCHIVES_HEAD_TITLE = u'Archives'
+NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
+NEST_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_ARCHIVES_HEADER_SUBTITLE = u'Posts in the Past'
+NEST_ARCHIVES_CONTENT_TITLE = u'Last Posts'
+
+## article.html
+NEST_ARTICLE_HEADER_BY = u'By'
+NEST_ARTICLE_HEADER_MODIFIED = u'modified'
+NEST_ARTICLE_HEADER_IN = u'in category'
+
+# author.html
+NEST_AUTHOR_HEAD_TITLE = u'Posts by'
+NEST_AUTHOR_HEAD_DESCRIPTION = u'Posts by'
+NEST_AUTHOR_HEADER_SUBTITLE = u'Posts archives'
+NEST_AUTHOR_CONTENT_TITLE = u'Posts'
+# authors.html
+NEST_AUTHORS_HEAD_TITLE = u'Author list'
+NEST_AUTHORS_HEAD_DESCRIPTION = u'Author list'
+NEST_AUTHORS_HEADER_TITLE = u'Author list'
+NEST_AUTHORS_HEADER_SUBTITLE = u'Archives listed by author'
+# categories.html
+NEST_CATEGORIES_HEAD_TITLE = u'Categories'
+NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
+NEST_CATEGORIES_HEADER_TITLE = u'Categories'
+NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+# category.html
+NEST_CATEGORY_HEAD_TITLE = u'Category Archive'
+NEST_CATEGORY_HEAD_DESCRIPTION = u'Category Archive'
+NEST_CATEGORY_HEADER_TITLE = u'Category'
+NEST_CATEGORY_HEADER_SUBTITLE = u'Category Archive'
+# pagination.html
+NEST_PAGINATION_PREVIOUS = u'Previous'
+NEST_PAGINATION_NEXT = u'Next'
+# period_archives.html
+NEST_PERIOD_ARCHIVES_HEAD_TITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEADER_TITLE = u'Archives'
+NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_CONTENT_TITLE = u'Archives for'
+# tag.html
+NEST_TAG_HEAD_TITLE = u'Tag archives'
+NEST_TAG_HEAD_DESCRIPTION = u'Tag archives'
+NEST_TAG_HEADER_TITLE = u'Tag'
+NEST_TAG_HEADER_SUBTITLE = u'Tag archives'
+# tags.html
+NEST_TAGS_HEAD_TITLE = u'Tags'
+NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
+NEST_TAGS_HEADER_TITLE = u'Tags'
+NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
+NEST_TAGS_CONTENT_TITLE = u'Tags List'
+NEST_TAGS_CONTENT_LIST = u'tagged'
+# Static files
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/logo.svg': {'path': 'logo.svg'}
+}
