386eeeb166c8ce4ad4b1a63a8239fb1741361c12
Hideyuki Teramura <hide4d51@gmail.com>
2020-08-15 17:48:48 +0900

    Revert URL prefix

--- webroot/publishconf.py
+++ webroot/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://testing.ikbk.net'
+SITEURL = 'https://ikbk.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
