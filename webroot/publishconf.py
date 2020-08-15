34880f8767e35332295d16a83a9d740a2aaf9a22
Hideyuki Teramura <hide4d51@gmail.com>
2020-08-15 11:50:35 +0900

    Temorarily change the URL prefix

--- webroot/publishconf.py
+++ webroot/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://ikbk.net'
+SITEURL = 'http://testing.ikbk.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
